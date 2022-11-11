6c0577a8c3e7383ae5492ac82e39851eafc9631b
thaonguyen <thaonguyen@gmail.com>
2022-11-12 01:42:29 +0700

    fix_2

--- src/service/LoginService.java
+++ src/service/LoginService.java
@@ -13,9 +13,9 @@ public class LoginService {
         IUserReponsitory iUserReponsitory = new UserRepository();
         IDonateReponsitory iDonateReponsitory = new DonateRepository();
         Scanner input = new Scanner(System.in);
-        System.out.print("Tài khoản 1234: ");
+        System.out.print("Tài khoản 321: ");
         String username = input.nextLine();
-        System.out.print("Mật khẩu  1234: ");
+        System.out.print("Mật khẩu  321: ");
         String password = input.nextLine();
         boolean check = true;
         if(!iUserReponsitory.findUserByUserNameAndPassword(username,password).checkExits()
